f24440e89ebcfac26966a50114be1c7530614fe2
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2019-11-07 09:10:22 +0700

    merge 07-Nov-2019

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<html>
-<head>
-    <title>Inwepo Sidebar</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-    </style>
-</head>
-<body>
-</body>
-</html>
-
 <?php
 /**
  * CodeIgniter
