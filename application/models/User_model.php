5bb369b49da1647aaf77359c285396d0a73c3388
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2019-11-07 09:14:47 +0700

    tambah model transaksi dan user

---
+++ application/models/User_model.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_model extends CI_Model
+{
+    public function select(Type $var = null)
+    {
+        # code...
+    }
+
+    public function select_where(Type $var = null)
+    {
+        # code...
+    }
+
+    public function update(Type $var = null)
+    {
+        # code...
+    }
+
+    public function delete(Type $var = null)
+    {
+        # code...
+    }
+}
+
+/* End of file User_model.php */
+
+?>
