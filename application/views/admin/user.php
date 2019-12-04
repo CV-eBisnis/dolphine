571af67f24805dd3b309d06ff6722cf2d2148dfc
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2019-12-05 04:58:05 +0700

    tambah cart

--- application/views/admin/user.php
+++ application/views/admin/user.php
@@ -13,10 +13,10 @@
                             <th>Nama</th>
                             <th>Alamat</th>
                             <th>No. Hp</th>
-                            <th><button data-toggle="modal" data-target="#ModalTambah"><i class="fa fa-plus"></i></button></th>
+                            <th><a data-toggle="modal" data-target="#modal_tambah" type="button" class="btn"><i class="fa fa-plus"></i></a></th>
 
                             <!-- Modal Tambah -->
-                            <div class="modal fade" id="ModalTambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="modal_tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
@@ -30,28 +30,28 @@
                                             <div class="modal-body">
                                                 <div class="form-group">
                                                     <label for="nama">Nama :</label>
-                                                    <input type="text" class="form-control" name="nama" id="nama" placeholder="Masukkan Nama ..." required minlength="5">
+                                                    <input type="text" class="form-control" name="nama" id="nama0" placeholder="Masukkan Nama ..." required minlength="5">
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="alamat">Alamat :</label>
-                                                    <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Masukkan Alamat ...">
+                                                    <input type="text" class="form-control" name="alamat" id="alamat0" placeholder="Masukkan Alamat ...">
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="no_hp">No. HP :</label>
-                                                    <input type="number" class="form-control" name="no_hp" id="no_hp" placeholder="Masukkan No. HP ...">
+                                                    <input type="number" class="form-control" name="no_hp" id="no_hp0" placeholder="Masukkan No. HP ...">
                                                 </div>
                                                 <hr>
                                                 <div class="form-group">
                                                     <label for="email"><b>Email :</b></label>
-                                                    <input type="email" class="form-control" name="email" id="email" placeholder="Masukkan Email ..."required>
+                                                    <input type="email" class="form-control" name="email" id="email0" placeholder="Masukkan Email ..."required>
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="password"><b>Password :</b></label>
-                                                    <input type="password" class="form-control" name="password" id="password" placeholder="Masukkan Password ..." required minlength="8">
+                                                    <input type="password" class="form-control" name="password" id="password0" placeholder="Masukkan Password ..." required minlength="8">
                                                 </div>
                                                 <div class="form-group">
                                                     <label for="level">Level :</label>
-                                                    <select class="form-control" name="level" id="level">
+                                                    <select class="form-control" name="level" id="level0">
                                                         <option>Administrator</option>
                                                         <option selected>User</option>
                                                     </select>
@@ -77,10 +77,10 @@
                                 <td><?= $u->alamat ?></td>
                                 <td><?= $u->no_hp ?></td>
                                 <td>
-                                    <a data-toggle="modal" data-target="#ModalEdit<?= $u->id_user ?>" type="button" class="btn"><i class="fa fa-pencil"></i></a>
+                                    <a data-toggle="modal" data-target="#modal_edit<?= $u->id_user ?>" type="button" class="btn"><i class="fa fa-pencil"></i></a>
                                     
                                     <!-- Modal Edit -->
-                                    <div class="modal fade" id="ModalEdit<?= $u->id_user ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal fade" id="modal_edit<?= $u->id_user ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
                                                 <div class="modal-header">
@@ -93,32 +93,32 @@
                                                     <div class="modal-body">
                                                         <div class="form-group">
                                                             <label for="id_user"><b>ID User : <?= $u->id_user ?></b></label>
-                                                            <input type="hidden" class="form-control" name="id_user" id="id_user" value="<?= $u->id_user ?>">
+                                                            <input type="hidden" class="form-control" name="id_user" id="id_user<?= $u->id_user ?>" value="<?= $u->id_user ?>">
                                                         </div>
                                                         <div class="form-group">
                                                             <label for="nama">Nama :</label>
-                                                            <input type="text" class="form-control" name="nama" id="nama" placeholder="Masukkan Nama ..." value="<?= $u->nama ?>" required minlength="5">
+                                                            <input type="text" class="form-control" name="nama" id="nama<?= $u->id_user ?>" placeholder="Masukkan Nama ..." value="<?= $u->nama ?>" required minlength="5">
                                                         </div>
                                                         <div class="form-group">
                                                             <label for="alamat">Alamat :</label>
-                                                            <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Masukkan Alamat ..." value="<?= $u->alamat ?>">
+                                                            <input type="text" class="form-control" name="alamat" id="alamat<?= $u->id_user ?>" placeholder="Masukkan Alamat ..." value="<?= $u->alamat ?>">
                                                         </div>
                                                         <div class="form-group">
                                                             <label for="no_hp">No. HP :</label>
-                                                            <input type="number" class="form-control" name="no_hp" id="no_hp" placeholder="Masukkan No. HP ..." value="<?= $u->no_hp ?>">
+                                                            <input type="number" class="form-control" name="no_hp" id="no_hp<?= $u->id_user ?>" placeholder="Masukkan No. HP ..." value="<?= $u->no_hp ?>">
                                                         </div>
                                                         <hr>
                                                         <div class="form-group">
                                                             <label for="email"><b>Email :</b></label>
-                                                            <input type="email" class="form-control" name="email" id="email" placeholder="Masukkan Email ..." value="<?= $u->email ?>"required>
+                                                            <input type="email" class="form-control" name="email" id="email<?= $u->id_user ?>" placeholder="Masukkan Email ..." value="<?= $u->email ?>"required>
                                                         </div>
                                                         <div class="form-group">
                                                             <label for="password"><b>Password :</b></label>
-                                                            <input type="password" class="form-control" name="password" id="password" placeholder="Masukkan Password ..." value="<?= $u->password ?>" required minlength="8">
+                                                            <input type="password" class="form-control" name="password" id="password<?= $u->id_user ?>" placeholder="Masukkan Password ..." value="<?= $u->password ?>" required minlength="8">
                                                         </div>
                                                         <div class="form-group">
                                                             <label for="level">Level :</label>
-                                                            <select class="form-control" name="level" id="level">
+                                                            <select class="form-control" name="level" id="level<?= $u->id_user ?>">
                                                                 <option <?php if($u->level==1) echo 'selected' ?> value="1">Administrator</option>
                                                                 <option <?php if($u->level==2) echo 'selected' ?> value="2">User</option>
                                                             </select>
